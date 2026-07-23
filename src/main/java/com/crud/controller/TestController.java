f22d0fafd4ab0180b9efcd9eeae4a75692a2833c
abdulsameer0 <abdulsameer9685@gmail.com>
2026-07-23 11:22:38 +0530

    CAS-23072026-added controller file for testing exception handler

---
+++ src/main/java/com/crud/controller/TestController.java
@@ -0,0 +1,45 @@
+package com.crud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController
+{
+               @GetMapping("/api1")
+               public String method(){
+                   try {
+                       int a = 10/0;
+
+                   }
+                 catch (Exception e) {
+
+                     return  e.getMessage();
+                 }
+                   return "d";
+               }
+
+
+    @GetMapping("/api2")
+    public String method2(){
+
+
+        try {
+            String a = null;
+            int b = a.length();
+
+            System.out.println(b);
+        }
+        catch (Exception e) {
+           return e.getMessage();
+        }
+
+        return null;
+    }
+
+    @GetMapping("/api3")
+    public String method3(){
+        return "This is a third method";
+
+    }
+}
