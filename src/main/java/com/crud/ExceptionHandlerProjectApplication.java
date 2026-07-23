3e4abc6ad9cef80fa0fcc159ea76790a9ce50b79
abdulsameer0 <abdulsameer9685@gmail.com>
2026-07-23 10:43:36 +0530

    CAS-23072026-this is a first commit

---
+++ src/main/java/com/crud/ExceptionHandlerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionHandlerProjectApplication.class, args);
+        System.out.println("this is main entry point core project create by sameer!");
+    }
+}
